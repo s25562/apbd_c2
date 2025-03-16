ac91a2f9e593a27f516e1e23f9eb1cbaebd4ac71
Kolumb <jakub.amerek@gmail.com>
2025-03-16 16:20:14 +0100

    AddExercise

---
+++ ConsoleApp1/ConsoleApp1/Inheritance/A.cs
@@ -0,0 +1,12 @@
+namespace ConsoleApp1.Inheritance;
+
+public class A
+{
+    private int number { get; set; }
+
+    public A(int number)
+    {
+        this.number = number;
+    }
+    
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+int number = 0;
+string input = "Text" + " jakiś inny tekst " + number;
+string input2 = $"Text: {number}";
+
+var zmienna = "Hello, World!";
+
+//Nullable
+{
+    int? nullableInt = null;
+    nullableInt = 10;
+    Object? o = null;
+}
+
+//Kolekcje
+{
+    var list = new List<int>();
+    list.Add(10);
+    
+    var dict = new Dictionary<string, int>();
+    dict.Add("1", 1);
+    
+}
+
+//Obsługa błędów
+{
+    try
+    {
+        throw new Exception();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
